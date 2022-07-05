fdeac3c608f210c43740f265959ca2d3db7c7c59
Jungsu <jungsu.cs@whatsoftdata.com>
2022-07-05 18:07:34 +0900

    not moving textboxes

--- draggable_rand_pos.js
+++ draggable_rand_pos.js
@@ -44,7 +44,7 @@ var mainArea = document.getElementById("main_area");
 var winWidth = mainArea.offsetWidth;
 var winHeight = mainArea.offsetHeight;
 $(window).on("load", function () {
-	$(".draggable").each(function () {
+	$(".draggable").not(".textbox").each(function () {
 		var y = Math.floor(Math.random() * (winHeight- this.offsetHeight));
 		var x = Math.floor(Math.random() * (winWidth - this.offsetWidth));
 		$(this).delay(1000).animate({ top: y + "px", left: x + "px" });
